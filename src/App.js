c65cb7cfd3f2cedec1b939ed4311d5cf96a4e101
Hassan Khan <40873451+hassankn@users.noreply.github.com>
2020-01-13 11:40:36 -0500

    add react-todo-app files

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import './App.css';
+import Todos from './components/Todos';
+import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
+import uuid from 'uuid';
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: uuid.v4(),
+        title: 'Make dinner',
+        completed: false
+      },
+      {
+        id: uuid.v4(),
+        title: 'Do laundry',
+        completed: false
+      },
+      {
+        id: uuid.v4(),
+        title: 'Go for a run',
+        completed: false
+      }
+    ]
+  }
+
+  // toggle complete
+  toggleComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    })
+  }
+
+  delTodo = (id) => {
+    this.setState({
+      todos: [...this.state.todos.filter(todo =>
+        todo.id !== id)]
+    });
+  }
+
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid.v4(),
+      title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo]})
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <div className="container">
+          <Header />
+          <AddTodo addTodo={this.addTodo}/>
+          <Todos todos={this.state.todos} toggleComplete={this.toggleComplete}
+            delTodo={this.delTodo} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
